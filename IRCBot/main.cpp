5812ec3ad1da37cdbb487fbe02f84959d9d44fe7
alireza <alirezafour@gmail.com>
2017-06-05 14:56:12 +0430

    Update main.cpp

--- IRCBot/main.cpp
+++ IRCBot/main.cpp
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-      IrcBot myBot("irc.freenode.net", 6667, "s3nder2", "s3nder2", "thisismind", "s3nder2");
-      myBot.Listen("irc.freenode.net", 6667, "s3nder2", "s3nder2", "thisismind", "s3nder2");
-      myBot.sendMessage("سلام من آمدم :D", "thisismind");
+      IrcBot myBot("irc.freenode.net", 6667, "s3nder2", "s3nder2", "sayhifromqt", "s3nder2");
+      myBot.Listen("irc.freenode.net", 6667, "s3nder2", "s3nder2", "sayhifromqt", "s3nder2");
+      myBot.sendMessage("سلام من آمدم :D", "sayhifromqt");
     return a.exec();
 }
